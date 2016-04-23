6bf5430f33f8e47dcc1052536143dd0ae2b80dea
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2016-04-23 11:33:07 -0600

    Added files via upload

---
+++ cameraControl.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var followMe:GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position.x = followMe.transform.position.x;
+}
---
+++ flight.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var speedMultiplier = 5;
+private var startPoint:Vector2;
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+	{
+		startPoint = Input.GetTouch(0).position;
+	}
+
+	if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended){
+		var end = Input.GetTouch(0).position;
+		GetComponent(Rigidbody2D).AddForce(Vector2((startPoint.x - end.x) * speedMultiplier,
+												   (startPoint.y - end.y) * speedMultiplier));
+	}
+}
